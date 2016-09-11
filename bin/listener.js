21f1390a64b3d6b2581023d1b4b3ff29f3f10729
damarrus <damarrus92@gmail.com>
2016-09-11 22:50:22 +0500

    хот фикс после неудачного мержа

--- bin/listener.js
+++ bin/listener.js
@@ -5,23 +5,16 @@
 function listenServer() {
     turn = getTurn();
     console.log(turn.status_turn);
-<<<<<<< HEAD
-    if (((turn.status_turn == 1 && player1.player_id == 2) || (turn.status_turn == 0 && player1.player_id == 1)) &&
-        turndop != turn.status_turn) {
-        turndop = turn.status_turn;
-=======
-    console.log(turndop);
-
     /*if (turn.status_turn == 1 && turndop == turn.status_turn) {
-        ++turn_counter;
-    }
-    console.log('turn counter ' + turn_counter);*/
+     ++turn_counter;
+     }
+     console.log('turn counter ' + turn_counter);*/
     if (turn.status_turn == 1 && turndop == 1) {
         console.log('hod');
         turndop = 0;
->>>>>>> origin/master
         card_activate();
         currentcard = getcard(turn.status_card_id);
         own_tower.useCard(currentcard);
     }
+
 }
