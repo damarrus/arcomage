d0bb2f606a7d526d10ff56a3ab68d9ee398d264f
DeRibura <deribura@mail.ru>
2016-09-11 19:58:56 +0500

    New file draw for drawing
    Отрисовка через отдельный файл

---
+++ bin/draw.js
@@ -0,0 +1,31 @@
+/**
+ * Created by DeRibura on 11.09.2016.
+ */
+
+function drawCardsStart() {
+
+    $('body').append('<div id = container class = container>');
+
+    for (var i = 0; i <= 6; ++i) {
+
+        card_obj[i] = new Card(card_id);
+        $('#container').append('<div id = card' + i + ' class = card><p>' + card_obj[i].card_name + '</p></div>');
+        card[i] = $('#card' + i);
+        $.extend(card[i], card_obj[i]);
+
+        card[i].click(function () {
+            if ((turn.status_turn == 0 && player1.player_id == 1) || (turn.status_turn == 1 && player1.player_id == 2)) {
+                var i = ($(this).attr('id')).substring(4, 5);
+                setTurn(player1.player_id);
+                setCurrentCard(card[i].card_id);
+                interval = setInterval(listenServer, 1000);
+                card_delete($(this), i);
+                var newCard = getNewCard();
+                renew(i * 1);
+                card_deactivate();
+
+            }
+        });
+    }
+
+}
--- bin/initgame.js
+++ bin/initgame.js
@@ -6,41 +6,16 @@
 
 var init = {
     initialize: function(){
+    setFirstTurn();
 
-        setFirstTurn();
+    drawCardsStart();
 
-        $('body').append('<div id = container class = container>');
+    if (player1.player_id == 2){
+        card_deactivate();
+    }
 
-        for (var i=0; i<=6; ++i){
-
-            card_obj[i] = new Card(card_id);
-            $('#container').append('<div id = card'+i+' class = card><p>'+card_obj[i].card_name+'</p></div>' );
-            card[i] = $('#card'+i);
-            $.extend(card[i], card_obj[i]);
-
-            card[i].click(function()
-            {
-                if ((turn.status_turn == 0 && player1.player_id == 1) || (turn.status_turn == 1 && player1.player_id == 2))
-                {
-                    var i = ($(this).attr('id')).substring(4, 5);
-                    setTurn(player1.player_id);
-                    setCurrentCard(card[i].card_id);
-                    interval = setInterval(listenServer, 1000);
-                    card_delete($(this),i);
-                    var newCard = getNewCard();
-                    renew(i*1);
-                    card_deactivate();
-
-                }
-            });
-        }
-
-        if (player1.player_id == 2){
-            card_deactivate();
-        }
-
-        player_tower = new Tower(true);
-        enemy_tower = new Tower(false);
+    player_tower = new Tower(true);
+    enemy_tower = new Tower(false);
 
     },
     auth: function(){
