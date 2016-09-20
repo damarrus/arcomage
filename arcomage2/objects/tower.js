7996bb6c8c688af4a0c9a91533e1e86c13ce0bb1
damarrus <damarrus92@gmail.com>
2016-09-20 22:28:50 +0500

    Исправил тавер

--- arcomage2/objects/tower.js
+++ arcomage2/objects/tower.js
@@ -8,36 +8,9 @@ function Tower(owner, callback){
     this.owner = owner;
     drawTower(owner, callback);
 
-
     function drawTower(owner, callback) {
         var own = (owner) ? 'own_tower' : 'enemy_tower';
-        $('#container2').append('<div id =' + own + ' class =' + own + '><p>'+ own + '<br>' + + self.health + '</p></div>');
+        $('#container2').append('<div id =' + own + ' class =' + own + '><p>'+ own + '<br>' + self.health + '</p></div>');
         callback();
     }
-
-    this.useCard = function(card){
-
-        if (owner)
-        {
-            this.health += card.card_self_tower_hp * 1;
-        }
-        else
-        {
-            enemy_tower.health += card.card_enemy_tower_hp * 1;
-        }
-        console.log("селф " + this.health);
-    };
-
-    this.useCardEnemy = function(card) {
-        if (!owner)
-        {
-            enemy_tower.health += card.card_self_tower_hp * 1;
-        }
-        else
-        {
-            this.health += card.card_enemy_tower_hp * 1;
-
-        }
-        console.log("енеми " + this.health);
-    };
 }
